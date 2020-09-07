74ab263504b60b12e97a4135225fa8c98392921d
Atul <atul.prasad.333@gmail.com>
2020-09-07 23:00:38 +0530

    added configuration child routes

--- src/router.js
+++ src/router.js
@@ -10,16 +10,18 @@ import Register from './components/Register';
 import FAQ from './components/faq/faq';
 import PrimitiveComponent from './components/primitives/primitives';
 import configuration from './components/configuration/configuration'
+import ReCreateDbTable from './components/configuration/recreateDbTable/recreateDbTable';
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
-        <Route path="/Register" component={Register} exact/>
-        <Route path="/faq" component={FAQ} exact/>
-        <Route path="/primitives" component={PrimitiveComponent} exact/>
-        <Route path="/configuration" component={configuration} exact/>
+        <Route path="/Register" component={Register} exact />
+        <Route path="/faq" component={FAQ} exact />
+        <Route path="/primitives" component={PrimitiveComponent} exact />
+        <Route path="/configuration" component={configuration} exact />
+        <Route path="/configuration/buildDbTable" component={ReCreateDbTable} exact />
       </Switch>
     </Router>
   )
